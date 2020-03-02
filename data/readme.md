decad716c7babee3d0c1573597eb6ea91d757924
DaihongChen <daihongchen@icloud.com>
2020-03-02 15:16:58 -0800

    Add sql tables

---
+++ data/readme.md
@@ -0,0 +1,9 @@
+create a new environment named kh-env
+
+create a new database named king_house:
+    log in existing databse with psql
+    run the command: `create database king_house;`
+    
+    
+ 
+
---
+++ src/sql/02_create_extr_rosale.sql
@@ -0,0 +1,27 @@
+create table extr_rpsale
+(
+    "ExciseTaxNbr" text,
+    "Major" text,
+    "Minor" text,
+    "DocumentDate" text,
+    "SalePrice" text,
+    "RecordingNbr" text,
+    "Volume" text,
+    "Page" text,
+    "PlatNbr" text,
+    "PlatType" text,
+    "PlatLot" text,
+    "PlatBlock" text,
+    "SellerName" text,
+    "BuyerName" text,
+    "PropertyType" text,
+    "PrincipalUse" text,
+    "SaleInstrument" text,
+    "AFForestLand" text,
+    "AFCurrentUseLand" text,
+    "AFNonProfitUse" text,
+    "AFHistoricProperty" text,
+    "SaleReason" text,
+    "PropertyClass" text,
+    "SaleWarning" text
+)
