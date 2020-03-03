b03f662bc299fe4b383427ffe2d30053c9367309
Kyle December <kyledecember@gmail.com>
2020-03-03 12:30:04 -0800

    added csv df creation

--- data/readme.md
+++ data/readme.md
@@ -1,9 +1,11 @@
-create a new environment named kh-env
+## download 4 files
+from https://info.kingcounty.gov/assessor/DataDownload/default.aspx
 
-create a new database named king_house:
-    log in existing databse with psql
-    run the command: `create database king_house;`
-    
-    
- 
+    - Real Property Sales.zip
+    - Parcel.zip
+    - Residential Building.zip
+    - Lookup.zip
 
+## unzip to CSV files
+## move CSV files to the /data folder in the repo 
+ these CSV files have already been added to .gitignore
--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -3,9 +3,33 @@ import sys
 module_path = os.path.abspath(os.path.join(os.pardir))
 if module_path not in sys.path:
     sys.path.append(module_path)
-
+    
+import numpy as np
 import pandas as pd
+import datetime
+
+def create_dfs(csv1, csv2, csv3, csv4):
+    relative_filename = os.path.abspath(os.path.join(os.pardir, 'data', csv1))
+    df1 = pd.read_csv(relative_filename)
+    
+    relative_filename = os.path.abspath(os.path.join(os.pardir, 'data', csv2))
+    df2 = pd.read_csv(relative_filename)
+    
+    relative_filename = os.path.abspath(os.path.join(os.pardir, 'data', csv3))
+    df3 = pd.read_csv(relative_filename, encoding='latin-1')
+    
+    relative_filename = os.path.abspath(os.path.join(os.pardir, 'data', csv4))
+    df4 = pd.read_csv(relative_filename)
+    
+    return df1, df2, df3, df4
 
 
-def download_data():
-    pass
+def sales_2019(df_in):
+    df = df_in
+    df['datetime'] = pd.to_datetime(df['DocumentDate'])
+    yr2020 = datetime.date(2020, 1, 1)
+    yr2018 = datetime.date(2018, 12, 31)
+    df = df[df['datetime'] > yr2018]
+    df = df[df['datetime'] < yr2020]
+            
+    return df
