5ce9513735593540ae1b5c322996e1721c827dec
Kyle December <kyledecember@gmail.com>
2020-03-03 10:56:25 -0800

    created new branch for csv data cleaning and analysis

---
+++ src/data_cleaning.py
@@ -0,0 +1,11 @@
+import os
+import sys
+module_path = os.path.abspath(os.path.join(os.pardir))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+import pandas as pd
+
+
+def download_data():
+    pass
